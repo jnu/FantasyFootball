5cfa6be5fc3644adeb00f89f18a72638d64f28af
Joseph Nudell <joenudell@gmail.com>
2015-08-09 13:20:52 -0700

    Add tests

--- test/scoring.jl
+++ test/scoring.jl
@@ -1,5 +1,6 @@
 using FantasyFootball
 
+# Offensive scoring
 @test score_off() == 0
 @test score_off(rush_yds=1) == 0
 @test score_off(rush_yds=10) == 1
@@ -8,5 +9,12 @@ using FantasyFootball
 @test score_off(rec_yds=10) == 1
 @test score_off(rec_td=1) == 6
 @test score_off(pass_yds=10) == 0
-@test score_off(pass_yds=100) == 4
+@test score_off(pass_yds=25) == 1
 @test score_off(pass_td=1) == 4
+@test score_off(pass_int=1) == -2
+@test score_off(sack=1) == 0
+@test score_off(fum_l=1) == -2
+@test score_off(pr_td=1) == 6
+@test score_off(kr_td=1) == 6
+
+@test score_off(rush_yds=10, rush_td=1, rec_yds=10, rec_td=1, pass_yds=25, pass_td=1, pass_int=1, sack=1, fum_l=1, pr_td=1, kr_td=1) == (1 + 6 + 1 + 6 + 1 + 4 + -2 + 0 + -2 + 6 + 6)
